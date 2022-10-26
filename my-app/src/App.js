16e49b77454823fb976c149ab2b97046696aef0d
Akinmyrza Kanat uulu <45089065+Akinmyrza@users.noreply.github.com>
2022-10-26 08:44:16 +0600

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './header';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h3>Hello World</h3>
+        <div>
+          <Header />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
